8b3ac16dae37b90206e93f2b5aad8100e007bd4c
Yuyuan <yuyuanduan@gmail.com>
2022-03-20 22:10:05 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid text-center">
+<div class="container">
+    <div class="row">
+<div class="col-md-6">
+  <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+
+</div>
+
+<div class="col-md-6">
+    <p>Terms and Conditions</p>
+    
+    </div>
+    
+    </div> <!-- row -->
+</div> <!-- container -->
+</footer>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
