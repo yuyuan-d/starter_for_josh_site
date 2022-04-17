e4041937ba0606ade4d6ba8080de2ae11a6550bf
Yuyuan <yuyuanduan@gmail.com>
2022-04-17 16:36:05 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -1,12 +1,19 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
 
     <section class="container-fluid aboutbg text-center">
-<div class="container">
-<h2>GAMES &amp; LEVEL DESIGNER</h2>
-<p class="particle">...and particle wizard</p>
-<p class="about-text">My  name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
-    I always tell a story.</p>
-    
+    <div class="container">
+            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
+        
+        </div><!-- container-->
+   
 <div class="blocker">
 
 </div>
