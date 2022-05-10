1788ea74aa886640adeab03eeb45df35f6c47e9b
Yuyuan <yuyuanduan@gmail.com>
2022-05-10 12:30:57 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,13 +2,15 @@
 <div class="container">
     <div class="row">
 <div class="col-md-6">
-  <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
-
+  <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
+  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
 </div>
 
 <div class="col-md-6">
-    <p>Terms and Conditions</p>
-    
+    <!-- <p>Terms and Conditions</p> -->
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
     </div>
     
     </div> <!-- row -->
